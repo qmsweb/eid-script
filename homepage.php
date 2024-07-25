8ccd4fb2027d5ff4a8c2b5740a3adb2d2f403149
qmsweb <118023498+qmsweb@users.noreply.github.com>
2024-07-25 07:18:40 +0400

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,23 @@
+  <div>
+  <center> 
+ <div class="clr"></div> 
+<!-- Google Analytics Code !-->  
+ <a href="index.php"><img src="img/logo.png" style="width:100px;"/></a>  
+ <div class="clr"></div>   
+  <h2 class="aid-text">
+  يقدم لكم موقعنا خدمة بطاقة المعايدة بمناسبة رمضان ، وكل عام وأنتم بالف خير ، يمكنك كتابة الإسم أسفله لإنشاء بطاقة خاصة بالإسم المدخل</h2>  
+  <div class="clr"></div><br>      
+  <form action="" method="post">
+    <fieldset class="uk-fieldset">
+        <div class="uk-margin">
+            <input class="uk-input inpu" type="text" name="fullname" placeholder="أدخل الإسم هنا ">
+        </div>
+        <div class="uk-margin">
+         <button type="submit" name="Send" class="uk-button button">عرض البطاقة </button>
+        </div>    
+    </fieldset>
+  </form>
+        <div class="clr"></div><br>  
+        <!-- Google Adsense Code !-->
+  </center>         
+  </div> 
